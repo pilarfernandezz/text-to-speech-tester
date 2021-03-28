a7177b687d8e94db907f7094e1b8200228e39ddb
Pilar Baptista Fernandez <pilarfernandez@MacBook-Pro-de-Pilar.local>
2021-03-28 18:31:03 -0300

    ajusts,

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     devServer: {
         disableHostCheck: true
+    },
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = "My Vue App";
+                return args;
+            })
     }
+
 }
